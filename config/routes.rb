c66a1bea75a86b16cb6a2acc8a784134fff33147
kitadanaoya <naoya0206k@yahoo.co.jp>
2017-10-18 14:37:44 +0000

    sessions controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,4 @@
 Rails.application.routes.draw do
-  get 'highlights/index'
-
-  get 'highlights/show'
-
-  get 'highlights/new'
-
-  get 'highlights/create'
-
-  get 'highlights/edit'
-
-  get 'highlights/update'
-
-  get 'highlights/destroy'
-
-  get 'books/index'
-
-  get 'books/show'
-
-  get 'books/new'
-
-  get 'books/create'
-
-  get 'books/edit'
-
-  get 'books/update'
-
-  get 'books/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
   
@@ -34,4 +6,8 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   delete '/signup', to: 'users#destroy'
   resources :users, only: [:index, :show, :edit, :update]
+  
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sesssions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
