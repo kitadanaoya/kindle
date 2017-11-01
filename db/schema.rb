011dbb9d87519b90ce6098388f0798f2b750b811
masanobbb <mk.bko56@gmail.com>
2017-11-02 00:45:45 +0900

    add highlight db table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015073346) do
+ActiveRecord::Schema.define(version: 20171018153022) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20171015073346) do
     t.string "genre"
     t.datetime "published_date"
     t.text "description"
+  end
+
+  create_table "highlights", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "content"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
