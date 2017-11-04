1093021abfebbf324ada15b709df4c6d1d88eb9d
masanobbb <mk.bko56@gmail.com>
2017-11-04 20:34:41 +0900

    fix conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
-  get '/highlights', to: 'highlights#new'
-  post '/highlights', to: 'highlights#create'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
   
+	get '/highlights', to: 'highlights#new'
+  post '/highlights', to: 'highlights#create'
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   delete '/signup', to: 'users#destroy'
   resources :users, only: [:index, :show, :edit, :update]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
 end
